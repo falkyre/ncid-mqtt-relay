b4cad63a6181f4e24e6006b10ea5d9ab349c3031
Sean Ostermann <falkyre@gmail.com>
2023-02-16 17:41:23 -0600

    Update regex to manage newest NCID SDK.  Include rich logging to replace print statements

--- ncid-relay.py
+++ ncid-relay.py
@@ -6,6 +6,18 @@ import re
 import paho.mqtt.client as mqtt
 import json
 import datetime
+import logging
+from rich.logging import RichHandler
+
+
+logging.basicConfig(
+    level="NOTSET",
+    format="%(message)s",
+    datefmt="[%X]",
+    handlers=[RichHandler(rich_tracebacks=True)]
+)
+
+log = logging.getLogger("rich")
 
 def parse_optional(ip_string, default_port, cast=(lambda x: x)):
     ip, _, port = ip_string.partition(':')
@@ -21,25 +33,29 @@ def parse_args():
     parser.add_argument('--mqtt_auth', type=str, default=None)
     return parser.parse_args()
 
-def incoming_call(client, topic, _date, _time, _line, _nmbr, _mesg, _name):
-    print("call", _date, _time, _line, _nmbr, _mesg, _name)
+def incoming_call(client, topic, _date, _time, _line, _nmbr, _mesg, _fnmbr, _ntype, _ctry, _loca, _cari, _name):
+    print("call", _date, _time, _line, _nmbr, _mesg, _name, _loca, _ctry, _ntype)
     data = {"date": _date,
             "time": _time,
             "line": _line,
             "nmbr": _nmbr,
             "mesg": _mesg,
             "name": _name,
+            "location": _loca,
+            "country": _ctry,
+            "type": _ntype,
             }
     d = datetime.datetime(int(_date[4:8]), int(_date[0:2]), int(_date[2:4]),
                         int(_time[0:2]), int(_time[2:4]))
     now = datetime.datetime.now()
-    print(data)
+    log.info(data)
     delta = abs(now - d)
     if delta.seconds < 300:
         client.publish(topic, json.dumps(data))
-        print("published")
+        log.info("published")
     else:
-        print("delta too large")
+        log.info("delta too large")
+
 
 actions = [
     (re.compile(
@@ -49,6 +65,11 @@ actions = [
      r'\*LINE\*([^*]+)'
      r'\*NMBR\*(\d+)'
      r'\*MESG\*([^*]+)'
+     r'\*FNMBR\*([^*]+)'
+     r'\*NTYPE\*([^*]+)'
+     r'\*CTRY\*([^*]+)'
+     r'\*LOCA\*([^*]+)'
+     r'\*CARI\*([^*]+)'
      r'\*NAME\*([^*]+)'
      r'\*$')
      , incoming_call),
@@ -70,14 +91,18 @@ def main(ncid_server, mqtt_server, mqtt_topic, mqtt_auth):
         s = socket.socket()
         try:
             s.connect((ncid_host, ncid_port))
-            print("connected")
+            log.info("connected")
             while True:
                 data = s.recv(1024).decode().strip()
-                print(data)
-                for regex, func in actions:
-                    match = regex.match(data)
-                    if match:
+                if "CID:" not in data:
+                    log.info(data)
+                else:
+                   log.info("Incoming caller id:  {}".format(data))
+                   for regex, func in actions:
+                      match = regex.match(data)
+                      if match:
                         func(client, mqtt_topic, *match.groups())
+
         except Exception as E:
             raise E
         finally:
